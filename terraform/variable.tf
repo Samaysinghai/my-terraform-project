0fd5e59a69777deae4c3f4d180cba2dbe12ae4ea
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2025-03-11 11:25:01 +0530

    Create variable.tf

---
+++ terraform/variable.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "The project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The region to deploy in"
+  type        = string
+}
+
+variable "zone" {
+  description = "The zone to deploy in"
+  type        = string
+}
