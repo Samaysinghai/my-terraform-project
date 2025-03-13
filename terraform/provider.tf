7597ea66160a9ec8f8fbd6a92f5d5881ed71dba6
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2025-03-13 10:51:45 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,8 @@
-
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  credentials = jsondecode(data.google_secret_manager_secret_version.terraform_service_account_secret.secret_data)
+}
+data "google_secret_manager_secret_version" "terraform_service_account_secret" {
+  secret = "first-sctmanager" # This is the secret name in Secret Manager
+} 
