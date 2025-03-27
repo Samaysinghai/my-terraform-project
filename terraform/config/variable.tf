48d9b8042fcecfdec79a0d8a1c37d63a0a2e6f4a
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2025-03-27 15:57:14 +0530

    Update variable.tf

--- terraform/config/variable.tf
+++ terraform/config/variable.tf
@@ -1,1 +1,20 @@
+variable "project_id" {
+  description = "The GCP project ID."
+  type        = string
+}
+
+variable "region" {
+  description = "The region where the VM will be created."
+  type        = string
+}
+
+variable "zone" {
+  description = "The zone where the VM will be created."
+  type        = string
+}
+
+variable "vm_name" {
+  description = "Name of the virtual machine."
+  type        = string
+}
 
