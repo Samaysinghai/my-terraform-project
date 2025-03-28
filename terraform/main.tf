113831fc37f158c57f93721a689b7b6b9aa11ddb
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2025-03-28 10:10:50 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,12 @@
 module "vm" {
   # for_each = toset([for i in range(var.vm_count) : "vm-${i + 1}"])  # Convert to a set of strings
-
+  
   source    = "./modules/vm"
+  image = var.image
+  machine_type = var.machine_type
   project_id = var.project_id
   region     = var.region
   zone       = var.zone
-  vm_name    = each.key  # Use the key from for_each (which is the vm name)
+  vm_name    = var.vm_name  # Use the key from for_each (which is the vm name)
 }
 
