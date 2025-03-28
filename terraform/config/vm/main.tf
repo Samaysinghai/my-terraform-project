ed23e0e02ebfea113bb7002aee748534d310d03d
Samaysinghai <88703605+Samaysinghai@users.noreply.github.com>
2025-03-28 18:50:02 +0530

    Update main.tf

--- terraform/config/vm/main.tf
+++ terraform/config/vm/main.tf
@@ -1,23 +1,16 @@
 resource "google_compute_instance" "vm" {
   name         = var.vm_name
-  machine_type = "n1-standard-1"
+  machine_type = var.machine_type
   zone         = var.zone
 
   boot_disk {
     initialize_params {
-      # Use image family instead of specific image version
-      image = "projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts"  # Image family for Ubuntu 20.04 LTS
+      image = var.image
     }
   }
 
   network_interface {
     network = "default"
-    access_config {
-      // This will assign an external IP
-    }
+    access_config {}
   }
-
-  tags = ["web", "app"]
 }
-
-
