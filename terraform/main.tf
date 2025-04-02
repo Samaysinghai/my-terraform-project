5f4a526759a597313ee50469a1156a592a62e69f
Samaysinghai <samayjain199@gmail.com>
2025-04-02 16:04:40 +0530

    files c\ontent changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,18 @@ module "vm" {
   # for_each = toset([for i in range(var.vm_count) : "vm-${i + 1}"])  # Convert to a set of strings
   
   source    = "./modules/vm"
-  image = var.image
-  machine_type = var.machine_type
-  project_id = var.project_id
-  region     = var.region
-  zone       = var.zone
-  vm_name    = var.vm_name  # Use the key from for_each (which is the vm name)
+  
+project_id = "fine-arbor-452306-t2"
+region     = "us-central1"
+zone       = "us-central1-b"
+vm_name = "my-vm"
+machine_type = "e2-medium"
+image        = "debian-9-stretch-v20191014"
+
+#image = var.image
+  #machine_type = var.machine_type
+  #project_id = var.project_id
+  #region     = var.region
+  #zone       = var.zone
+  #vm_name    = var.vm_name  # Use the key from for_each (which is the vm name)
 }
